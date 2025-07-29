a4abca50b52ca64e1252f5a08c24bc78f29e0581
Dibier Marin <marindibier7@gmail.com>
2025-07-29 10:28:07 -0500

    Remover web innecesario.

--- web/with/fuente/fuente.js
+++
@@ -1,7 +0,0 @@
-// fuente.js
-
-// Crear e insertar el enlace de la fuente EB Garamond
-const link = document.createElement("link");
-link.rel = "stylesheet";
-link.href = "https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap";
-document.head.appendChild(link);
