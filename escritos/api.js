fa6513ef5fa5bc5cd906e6ef9a6c1d2383e9f1e8
Dibier Marin <marindibier7@gmail.com>
2025-08-11 10:09:36 -0500

    Apertura de entrada sobre emociones.

--- escritos/api.js
+++ escritos/api.js
@@ -1,3 +1,3 @@
-cargarComponente("escritos/triangulos-causales", "entrada-enlace[src='escritos/conjuntos']");
-cargarComponente("escritos/triangulos-causales", "entrada-enlace[src='escritos/emociones']");
+cargarComponente("escritos/conjuntos", "entrada-enlace[src='escritos/conjuntos']");
+cargarComponente("escritos/emociones", "entrada-enlace[src='escritos/emociones']");
 cargarComponente("escritos/triangulos-causales", "entrada-enlace[src='escritos/triangulos-causales']");
