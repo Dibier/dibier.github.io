21f60d4d3058c9a6a46cc1ad88f6a7ba109c860e
Dibier Marin <marindibier7@gmail.com>
2025-09-30 10:13:39 -0500

    Crear oración humildad.

--- escritos/api.js
+++ escritos/api.js
@@ -1,3 +1,4 @@
+cargarComponente("escritos/humildad", "entrada-enlace[src='escritos/humildad']");
 cargarComponente("escritos/conjuntos", "entrada-enlace[src='escritos/conjuntos']");
 cargarComponente("escritos/emociones", "entrada-enlace[src='escritos/emociones']");
 cargarComponente("escritos/triangulos-causales", "entrada-enlace[src='escritos/triangulos-causales']");
